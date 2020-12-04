6a685e0b984e4b725083e6886f02e811ea769c17
JohnWesleyK <59068112+JohnWesleyK@users.noreply.github.com>
2020-12-04 16:36:04 +0000

    Finished PartA in day4

--- day4/ex1.py
+++ day4/ex1.py
@@ -1,8 +1,30 @@
+def parse(lines):
+    parsed_data = {}
+    for line in lines:
+        if line.strip():
+            for word in line.split():
+                if ':' in word:
+                    key, value = word.split(':', 1)
+                    parsed_data[key] = value
+        else:
+            yield parsed_data
+            parsed_data = {}
+    yield parsed_data
+
 f = open('input.txt', 'r')
 data = [line.strip() for line in f.readlines()]
 
-i=1
-batch_file = data.split("\n")
+batch_file = parse(data)
+
+valid_passsports = 0
 
 for line in batch_file:
-    print(line)
+    if(all(valid_fields in line for valid_fields in ('eyr', 'hgt', 'byr', 'iyr', 'pid','hcl', 'ecl',))):
+        valid_passsports+=1
+        print("------Valid-Passport------")
+        print(line)
+    else:
+        print("------Invalid-Passport------")
+        print(line)
+
+print("Total Valid Passports =", valid_passsports)
